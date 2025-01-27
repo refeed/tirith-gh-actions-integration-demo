1aac15a0aa0fe78acea237423e6286a9268462e8
Rafid Aslam <rafid.aslam@stackguardian.io>
2025-01-27 19:28:06 +0700

    Add public-read ACL to S3 bucket resource in main.tf

--- main.tf
+++ main.tf
@@ -2,6 +2,12 @@ provider "aws" {
   region = "us-west-2"
 }
 
+resource "aws_s3_bucket_acl" "example" {
+  bucket = aws_s3_bucket.example_bucket.id
+
+  acl = "public-read"
+}
+
 resource "aws_s3_bucket" "example_bucket" {
   bucket = "example-bucket-name"
   acl    = "private"
